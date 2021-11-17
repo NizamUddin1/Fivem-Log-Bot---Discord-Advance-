b5469d2bb41046d0d069d6088b33b83c5b9d4bbc
nizam uddin <30291480+NizamUddin1@users.noreply.github.com>
2021-11-17 21:39:33 +0600

    Update

---
+++ server/server.lua
@@ -0,0 +1,85 @@
+local nuidev = {
+    ['default'] = '', --Paste Your WebHook URl
+    ['testwebhook'] = '',
+    ['playermoney'] = '',
+    ['playerinventory'] = '',
+    ['robbing'] = '',
+    ['cuffing'] = '',
+    ['drop'] = '',
+    ['trunk'] = '',
+    ['stash'] = '',
+    ['glovebox'] = '',
+    ['banking'] = '',
+    ['vehicleshop'] = '',
+    ['vehicleupgrades'] = '',
+    ['shops'] = '',
+    ['dealers'] = '',
+    ['storerobbery'] = '',
+    ['bankrobbery'] = '',
+    ['powerplants'] = '',
+    ['death'] = '',
+    ['joinleave'] = '',
+    ['ooc'] = '',
+    ['report'] = '',
+    ['me'] = '',
+    ['pmelding'] = '',
+    ['112'] = '',
+    ['bans'] = '',
+    ['anticheat'] = '',
+    ['weather'] = '',
+    ['moneysafes'] = '',
+    ['bennys'] = '',
+    ['bossmenu'] = '',
+    ['robbery'] = '',
+    ['casino'] = '',
+    ['traphouse'] = '',
+    ['911'] = '',
+    ['palert'] = '',
+}
+
+local Colors = { -- https://www.spycolor.com/
+    ['default'] = 14423100,
+    ['blue'] = 255,
+    ['red'] = 16711680,
+    ['green'] = 65280,
+    ['white'] = 16777215,
+    ['black'] = 0,
+    ['orange'] = 16744192,
+    ['yellow'] = 16776960,
+    ['pink'] = 16761035,
+}
+
+
+
+local QBCore = exports['qb-core']:GetCoreObject()
+
+
+
+
+RegisterNetEvent('qb-log:server:CreateLog', function(name, title, color, message, tagEveryone)        
+    local tag = tagEveryone or false
+    local nuidev = nuidev[name] or nuidev['default']
+    local embedData = {
+        {
+            ['title'] = title,
+            ['color'] = Colors[color] or Colors['default'],
+            ['footer'] = {
+                ['text'] = os.date('%c'),
+            },
+            ['description'] = message,
+            ['author'] = {
+                ['name'] = 'nu logs',
+                ['icon_url'] = 'https://cdn.discordapp.com/attachments/904925524156121088/910554542473641994/alf_badge_004_diff.png',
+            },
+        }
+    }
+    PerformHttpRequest(nuidev, function(err, text, headers) end, 'POST', json.encode({ username = 'nu logs', embeds = embedData}), { ['Content-Type'] = 'application/json' })
+    Citizen.Wait(100)
+    if tag then
+        PerformHttpRequest(nuidev, function(err, text, headers) end, 'POST', json.encode({ username = 'nu logs', content = '@everyone'}), { ['Content-Type'] = 'application/json' })
+    end
+end)
+
+QBCore.Commands.Add('testwebhook', 'Test Your Discord Webhook For Logs ( GOD )', {}, false, function(source, args)
+    TriggerEvent('qb-log:server:CreateLog', 'testwebhook', 'Test Webhook', 'default', 'Webhook setup successfully')
+end, 'god')
